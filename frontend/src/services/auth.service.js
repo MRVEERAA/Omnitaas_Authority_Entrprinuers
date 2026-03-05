c9ab7945dba548740acdb5618919b1d7fd0b9106
MRVEERAA <vasanthakumarssp@gmail.com>
2026-03-05 09:29:03 +0530

    Base Url Change

--- frontend/src/services/auth.service.js
+++ frontend/src/services/auth.service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000";
+const BASE_URL = "https://omnitaas-login-app.onrender.com";
 
 // Login (both modes)
 export const login = async (mode, username, password) => {
